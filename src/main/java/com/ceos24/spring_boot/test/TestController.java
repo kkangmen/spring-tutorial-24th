83447a998fe3fc221726e93c41fdd91b313000c7
kkangmen <pickofee@gmail.com>
2026-09-05 21:13:08 +0900

    feat: 1주차 스프링 튜토리얼

---
+++ src/main/java/com/ceos24/spring_boot/test/TestController.java
@@ -0,0 +1,21 @@
+package com.ceos24.spring_boot.test;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/tests")
+public class TestController {
+
+    private final TestService testService;
+
+    @GetMapping
+    public List<Test> findAllTest(){
+        return testService.findAllTests();
+    }
+}
---
+++ src/test/java/com/ceos24/spring_boot/HelloControllerTest.java
@@ -0,0 +1,32 @@
+package com.ceos24.spring_boot;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class HelloControllerTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @Test
+    @DisplayName("HellowController의 hello() 메서드 테스트")
+    public void getHello() throws Exception {
+        mvc.perform(get("/"))
+                .andExpect(status().isOk())
+                .andExpect(result -> {
+                    String response = result.getResponse().getContentAsString();
+                    assert response.equals("Hello, Spring Boot!");
+                });
+    }
+}
